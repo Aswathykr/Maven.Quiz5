c52b3047ff1dfadcdc487c9eba7b31fd33e8f3aa
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-04-08 10:57:30 -0400

    Object Orientation implemented and tested

--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/PalindromeObject.java
@@ -1,21 +1,26 @@
 package rocks.zipcode.io.quiz4.objectorientation;
 
+import rocks.zipcode.io.quiz4.fundamentals.PalindromeEvaluator;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeObject {
+
+    String palindrome;
     public PalindromeObject(String input) {
+        this.palindrome = input;
     }
 
     public String[] getAllPalindromes(){
-        return null;
+        return PalindromeEvaluator.getAllPalindromes(palindrome);
     }
 
     public Boolean isPalindrome(){
-        return null;
+        return PalindromeEvaluator.isPalindrome(palindrome);
     }
 
     public String reverseString(){
-        return null;
+        return PalindromeEvaluator.reverseString(palindrome);
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
+++ src/main/java/rocks/zipcode/io/quiz4/objectorientation/StringAssembler.java
@@ -4,14 +4,18 @@ package rocks.zipcode.io.quiz4.objectorientation;
  * @author leon on 11/12/2018.
  */
 public class StringAssembler {
+    Character delimiter ;
+    String output = "";
     public StringAssembler(Character delimeter) {
+        this.delimiter = delimeter;
     }
 
     public StringAssembler append(String str) {
-        return null;
+        output += str + delimiter;
+        return this;
     }
 
     public String assemble() {
-        return null;
+        return output.substring(0, output.length()-1);
     }
 }
