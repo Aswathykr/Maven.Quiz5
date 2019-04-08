5149ac00ffdd17fce212624f8d7c70ad9c649728
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-04-08 10:09:11 -0400

    fundementals implemented and tested

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,37 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        Set<String> palindromes = new HashSet<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i+1; j < string.length(); j++) {
+                String subStr = string.substring(i, j);
+                if(isPalindrome(subStr)){
+                    palindromes.add(subStr);
+                }
+            }
+        }
+        if(isPalindrome(string)){
+            palindromes.add(string);
+        }
+        return palindromes.toArray(new String[palindromes.size()]);
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        int halfLength =  string.length()/2;
+        String firstHalf = string.substring(0, halfLength);
+        return firstHalf.equals(reverseString(string.substring(string.length() - halfLength)));
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder builder = new StringBuilder(string);
+        builder.reverse();
+        return builder.toString();
     }
 }
