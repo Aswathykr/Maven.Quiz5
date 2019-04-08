36726bb1d44c64c69d2c531fb7e7c15743b4c98b
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-04-08 09:34:50 -0400

    collections implemented and tested

--- src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
@@ -4,17 +4,21 @@ package rocks.zipcode.io.quiz4.collections;
  * @author leon on 11/12/2018.
  */
 public class Student {
+    Integer id ;
+    Double totalStudyTime = 0.0;
     public Student() {
         this(null);
     }
 
     public Student(Integer id) {
+        this.id = (id == null) ? 0 : id;
     }
 
     public void learn(Double amountOfHours) {
+        totalStudyTime += amountOfHours;
     }
 
     public Double getTotalStudyTime() {
-        return null;
+        return totalStudyTime;
     }
 }
